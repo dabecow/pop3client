b40237f633fbaa13fb32477d4707969ae6556189
Andrei Bikov <andrey07ey@gmail.com>
2020-10-11 17:03:05 +0300

    Finish controller layer

--- src/edu/networks/pop3/client/controller/ClientController.java
+++ src/edu/networks/pop3/client/controller/ClientController.java
@@ -2,6 +2,7 @@ package edu.networks.pop3.client.controller;
 
 import edu.networks.pop3.client.model.Client;
 import java.io.IOException;
+import java.net.SocketTimeoutException;
 import java.util.Base64;
 
 public class ClientController {
@@ -12,14 +13,15 @@ public class ClientController {
   }
 
   public void sendMessage(String message){
-    System.out.println("[CLIENT] " + message);
     client.getWriter().println(message);
-    client.appendToLog("[CLIENT]" + message + "\n");
+    System.out.println("[CLIENT] " + message);
+    client.appendToLogLn("[CLIENT]" + message);
   }
 
   public String receiveAnswer() throws IOException {
     String answer = client.getIn().readLine();
-    client.appendToLog("[SERVER] " + answer + "\n");
+    client.appendToLogLn("[SERVER] " + answer);
+    System.out.println("[SERVER] " + answer);
     return answer;
   }
 
@@ -37,7 +39,50 @@ public class ClientController {
     return receiveAnswer().toLowerCase().startsWith("+ok");
   }
 
+  public String getMailBoxStatistics() throws IOException {
+    sendMessage("STAT");
+    return receiveAnswer();
+  }
 
+  public String getMailBoxList() throws IOException {
+    sendMessage("LIST");
+    return receiveAnswer();
+  }
+
+  public String getMailBoxList(String messageNumber) throws IOException {
+    sendMessage("LIST " + messageNumber);
+    return receiveAnswer();
+  }
+
+  public String getMessage(String number) throws IOException {
+    sendMessage("RETR " + number);
+    return receiveAnswer();
+  }
+
+  public boolean deleteMessage(String number) throws IOException {
+    sendMessage("DELE " + number);
+    return receiveAnswer().startsWith("+OK");
+  }
+
+  public boolean checkConnection() throws IOException {
+    sendMessage("NOOP");
+    client.getSocket().setSoTimeout(500);
+    try {
+      return receiveAnswer().startsWith("+OK");
+    } catch (SocketTimeoutException e) {
+      return false;
+    }
+  }
+
+  public boolean resetMessagesMarks() throws IOException {
+    sendMessage("RSET");
+    return receiveAnswer().startsWith("+OK");
+  }
+
+  public String getMessageLines(String messageNumber, String lines) throws IOException {
+    sendMessage("TOP " + messageNumber + " " + lines);
+    return receiveAnswer();
+  }
 
 }
   
